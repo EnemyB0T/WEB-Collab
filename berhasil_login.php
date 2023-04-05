7fc70e50101c9f8f1dc05886f407ded6292ea994
EnemyB0T <rampenj@hotmail.com>
2023-04-05 22:16:41 +0700

    connection to the database established
    Removal of .js part in noteAddis.php

--- berhasil_login.php
+++ berhasil_login.php
@@ -22,14 +22,21 @@ header("Refresh: 1; url=noteAddis.php");
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-
-    <link rel="stylesheet" type="text/css" href="style.css">
-    <title>Berhasil Login</title>
+    <link rel="stylesheet" href="Berhasil_login.css">
+    <title>Login page in HTML</title>
 </head>
 <body>
-    <div class="container-logout">
+    <h1 class="form-header">Quill</h1>
+    <form action="">
+        <!-- Headings for the form -->
+        <div class="headingsContainer">
+            <h3>Login</h3>
+            
+        </div>
+
+        <!-- Main container for all inputs -->
         <form action="" method="POST" class="login-email">
             <?php echo "<h1>Selamat Datang, " . $username ."!". "</h1>"; ?>
 
@@ -37,6 +44,7 @@ header("Refresh: 1; url=noteAddis.php");
             <a href="logout.php" class="btn">Logout</a>
             </div>
         </form>
-    </div>
+
+    </form>
 </body>
 </html>
--- noteAddis.php
+++ noteAddis.php
@@ -85,17 +85,19 @@ mysqli_close($conn);
             </h1>
             <div class="card">
                 <div class="card-body">
-                    <h5 class="card-title">Add title</h5>
+                    <form method="post" action="addnote.php">
+                        <h5 class="card-title">Add title</h5>
 
-                    <div class="form-group my-3">
-                        <input type="text" class="form-control" id="addTitle" placeholder="Enter your title" name="title">
-                    </div>
+                        <div class="form-group my-3">
+                            <input type="text" class="form-control" id="addTitle" placeholder="Enter your title" name="title">
+                        </div>
 
-                    <h5 class="card-title">Add a note</h5>
-                    <div class="form-group">
-                        <textarea class="form-control" id="addTxt" rows="3" placeholder="Enter your Note" name="content"></textarea>
-                    </div>
-                    <button class="btn btn-success my-3" id="addBtn" name="submit">Add Note</button>
+                        <h5 class="card-title">Add a note</h5>
+                        <div class="form-group">
+                            <textarea class="form-control" id="addTxt" rows="3" placeholder="Enter your Note" name="content"></textarea>
+                        </div>
+                        <button class="btn btn-success my-3" id="addBtn" name="submit">Add Note</button>
+                    </form> 
                 </div>
             </div>
             <hr>
@@ -127,12 +129,7 @@ mysqli_close($conn);
 
 
 
-    <!-- Bootstrap js -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-        crossorigin="anonymous"></script>
-
-    <script src="index.js"></script>
+    
 
     <button onclick="logout()">Logout</button>
 
