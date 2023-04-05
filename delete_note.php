d599b7b96989b65210588bcf4fd62cb843b00811
EnemyB0T <rampenj@hotmail.com>
2023-04-05 23:27:28 +0700

    Delete is now working
    Added a working delete button

--- delete_note.php
+++ delete_note.php
@@ -1,25 +1,45 @@
 <?php
+session_start();
+include('config.php');
 
-//Define the query
-$query = "DELETE FROM note WHERE noteID={$_POST['noteID']}";
+/*
+if(isset($_GET['id'])) {
+    $noteID = $_GET['id'];
+    echo "Note ID: $noteID";
+}
+else
+{
+    echo "Note ID not found";
+}
 
-//sends the query to delete the entry
-mysqli_query($conn, $query);
+*/
 
-if (mysqli_affected_rows($conn) == 1) { 
-//if it updated
-?>
+if (isset($_SESSION['userID']) && isset($_GET['noteID'])) {
+    $userID = $_SESSION['userID'];
+    $noteID = $_GET['noteID'];
 
-            <strong>Note Has Been Deleted</strong><br /><br />
+    // check if the note belongs to the user
+    $sql = "SELECT * FROM note WHERE noteID=$noteID AND userID=$userID";
+    $result = mysqli_query($conn, $sql);
+
+    if (mysqli_num_rows($result) == 1) {
+        // delete the note
+        $sql = "DELETE FROM note WHERE noteID=$noteID";
+        $result = mysqli_query($conn, $sql);
+
+        if ($result) {
+            echo '<p>Note deleted successfully!</p>';
+            header('Location: noteAddis.php');
+        } else {
+            echo '<p>Error deleting note: ' . mysqli_error($conn) . '</p>';
+        }
+    } else {
+        echo '<p>You do not have permission to delete this note.</p>';
+    }
     
-<?php
- } else { 
-//if it failed
-?>
-    
-            <strong>Deletion Failed</strong><br /><br />
-    
+} else {
+    echo '<p>Invalid request.</p>';
+}
 
-<?php
-} 
+mysqli_close($conn);
 ?>
